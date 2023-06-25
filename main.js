4519bd6b128c592aaf1b47d63a68ff8c5b368a58
miguel <m.miguelvasquez11@gmail.com>
2023-06-25 13:25:34 -0500

    sport

---
+++ main.js
@@ -0,0 +1,14 @@
+// function actualizarTexto() {
+//   var anchoPantalla = window.innerWidth;
+//   var enlace = document.getElementById("list__sp");
+
+//   if (anchoPantalla < 800) {
+//     enlace.textContent = "Deportes móviles";
+//   } else {
+//     enlace.textContent = "Sports";
+//   }
+// }
+
+// actualizarTexto();
+
+// window.addEventListener("resize", actualizarTexto);
